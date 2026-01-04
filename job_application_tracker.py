c4a34c2590014f8992356feb96d2e2bf390e5f26
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-01-04 20:36:14 +0000

    Set up flask, landing page and login page (both functionally incomplete)

---
+++ job_application_tracker.py
@@ -0,0 +1,6 @@
+from app import app
+
+# The following block of code is not part of the tutorial
+# I have added it to make running and debugging the app simpler
+if __name__ == "__main__":
+    app.run(debug=True)
