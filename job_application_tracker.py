829ee8f3139b1fceed9d2d877efcd632b4d87c0f
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-01-30 15:51:03 +0000

    Added 'Job' model to models.py and associated table to database, as well as password hashing functions to the 'User' model in models.py

--- job_application_tracker.py
+++ job_application_tracker.py
@@ -4,3 +4,12 @@ from app import app
 # I have added it to make running and debugging the app simpler
 if __name__ == "__main__":
     app.run(debug=True)
+
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Job
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Job': Job}
