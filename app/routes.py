55f4a3457362b78eda4f3921f842f2a9e505990f
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-03-04 16:32:06 +0000

    Implemented fully functioning 'User Login and Sign up' system

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 import sqlalchemy as sa
+from urllib.parse import urlsplit
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html', title='Job Application Tracker')
 
@@ -20,13 +22,36 @@ def login():
             sa.select(User).where(User.username == form.username.data)
         )
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid username or password', 'error')
             return redirect(url_for('login'))
         login_user(user, remember = form.remember_me.data)
-        return redirect(url_for(index))
-    return render_template('login.html', title='Sign In', form=form)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Log In', form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for(index))
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+# atm rough code, check if errors pop up
+@app.route('/applications')
+@login_required
+def applications():
+    return render_template('applications.html', title='Applications')
